c4ce018af9c12b7ce19b7461007ed3952dcc9a51
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-29 23:27:11 +0300

    #2 create auth dto

---
+++ src/controller/auth.dto.ts
@@ -0,0 +1,14 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+export class SignUpDto {
+    login: string;
+    email: string;
+    password: string;
+}
+
+export class SignInDto {
+    login: string;
+    password: string;
+}
