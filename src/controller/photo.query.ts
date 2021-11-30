912da6bffaa6b3457900933a205767a53a80f030
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 12:17:28 +0300

    #5 PhotoDeleteQuery dto

--- src/controller/photo.query.ts
+++ src/controller/photo.query.ts
@@ -1,9 +1,15 @@
 /**
  * Author: SyntaxErrorLineNULL.
  */
+import { IsNumber } from 'class-validator';
 
 export class PhotoQuery {
     ownerId?: string;
     page?: number;
     maxCount?: number;
+}
+
+export class PhotoDeleteQuery {
+    @IsNumber()
+    photoId: number;
 }
