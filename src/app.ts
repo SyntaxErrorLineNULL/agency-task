5628123608811e25c3871510a121b2c64cbca865
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 10:49:21 +0300

    mongodb connect, use container, express server option

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@
 
 import express from 'express';
 import { Action, useContainer, useExpressServer } from 'routing-controllers';
+import { connect } from 'mongoose';
+import 'reflect-metadata';
+import { Container } from 'typedi';
 
 export class App {
     public app: express.Application;
@@ -13,8 +16,17 @@ export class App {
     }
 
     public async createExpressServer(port: number) {
+        try {
+            connect('mongodb://127.0.0.1:27017/app',() => console.log('Mongoose is connected'));
+        } catch (e) {
+            console.log(e);
+        }
+        useContainer(Container);
         useExpressServer(this.app, {
             controllers: [],
+            authorizationChecker: async (action: Action, roles?: string[]): Promise<boolean> => {
+                return true;
+            }
         });
         this.app.listen(port, () => console.log(`Running on port ${port}`));
     }
