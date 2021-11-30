f1ca1f1513700fd12cd216aa44cb746b039a2066
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 11:49:15 +0300

    #4 create get photos method

--- src/controller/photo.controller.ts
+++ src/controller/photo.controller.ts
@@ -2,21 +2,24 @@
  * Author: SyntaxErrorLineNULL.
  */
 
-import { JsonController, Get } from 'routing-controllers';
-import { PhotoService } from '../module/photo/photo.service';
+import { JsonController, Get, QueryParams } from 'routing-controllers';
 import { Service } from 'typedi';
 import axios from 'axios';
 import Album, { IAlbum } from '../module/album/schema/album.schema';
 import User, { IUser } from '../module/user/schema/user.schema';
 import Photo, { IPhoto } from '../module/photo/schema/photo.schema';
+import { PhotoQuery } from './photo.query';
+import { PhotoDto } from './model.dto';
+import { ModelMapper } from './model.mapper';
 
 @JsonController('')
 @Service()
 export class PhotoController {
+    constructor(private mapper: ModelMapper) {}
 
     @Get('/load-photos')
     async loadPhoto(): Promise<string> {
-        const user: IUser = await User.findById('61a5da9b74d312a2641cf06b');
+        const user: IUser = await User.findById('61a5e478d051076645fadf64');
         try {
             const { data } = await axios.get('http://jsonplaceholder.typicode.com/photos');
             for (let i of data) {
@@ -33,6 +36,18 @@ export class PhotoController {
                 await photo.save();
             }
             return 'success';
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    @Get('/get-photos')
+    async getPhoto(@QueryParams() query: PhotoQuery): Promise<PhotoDto[]> {
+        try {
+            const user: IUser = await User.findById('61a5e478d051076645fadf64');
+            const data: IPhoto[] = await Photo.find({ owner: user._id })
+                .skip((query.page * query.maxCount));
+            return await Promise.all(data.slice(0, query.maxCount).map((photo: IPhoto) => this.mapper.photoMap(photo)));
         } catch (e) {
             console.log(e);
         }
