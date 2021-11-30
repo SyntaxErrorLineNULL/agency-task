c1d85352251c372a64d46f36e2e1aff821afefc3
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 13:43:17 +0300

    #8 remove exception static owner method

--- src/module/auth/exceptions.ts
+++ src/module/auth/exceptions.ts
@@ -6,8 +6,4 @@ export class AuthorizationException extends Error {
     public static wrongCredentials() {
         return new AuthorizationException('Неверный email, логин или пароль');
     }
-
-    public static wrongOwner() {
-        return new AuthorizationException('Вы не являетесь владельцем фоторгафии');
-    }
 }
