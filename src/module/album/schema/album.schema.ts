862a8ff369862c7f4d700b547fd415666d79a59e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 13:34:47 +0300

    #6 fix album.schema.ts add id(album id)

--- src/module/album/schema/album.schema.ts
+++ src/module/album/schema/album.schema.ts
@@ -6,11 +6,13 @@ import { Document, Model, model, Schema } from 'mongoose';
 import { IUser } from '../../user/schema/user.schema';
 
 export interface IAlbum extends Document {
+    id: string;
     title: string;
     owner: IUser;
 }
 
 const AlbumSchema: Schema = new Schema({
+    id: { type: String },
     title: { type: String },
     owner: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
 });
