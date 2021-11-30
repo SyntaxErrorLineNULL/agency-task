9da365e630e93a7606d3f183b58d0198dc83d287
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 11:54:25 +0300

    #5 wrongOwner exception

--- src/module/auth/exceptions.ts
+++ src/module/auth/exceptions.ts
@@ -6,4 +6,8 @@ export class AuthorizationException extends Error {
     public static wrongCredentials() {
         return new AuthorizationException('Неверный email, логин или пароль');
     }
+
+    public static wrongOwner() {
+        return new AuthorizationException('Вы не являетесь владельцем фоторгафии');
+    }
 }
