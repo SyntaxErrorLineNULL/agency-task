c5041adb2821aa417073dc1a533cca9028bb545e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 11:27:26 +0300

    #4 fix album schema

--- src/module/album/schema/album.schema.ts
+++ src/module/album/schema/album.schema.ts
@@ -6,13 +6,11 @@ import { Document, Model, model, Schema } from 'mongoose';
 import { IUser } from '../../user/schema/user.schema';
 
 export interface IAlbum extends Document {
-    id: number;
     title: string;
     owner: IUser;
 }
 
 const AlbumSchema: Schema = new Schema({
-    id: { type: Number },
     title: { type: String },
     owner: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
 });
