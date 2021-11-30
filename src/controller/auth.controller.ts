1b6bbd1b499acaf0e7dfb9d29c292fbff73ba8b3
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-30 10:57:35 +0300

    #2 create auth controller register method

---
+++ src/controller/auth.controller.ts
@@ -0,0 +1,35 @@
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+import { Body, JsonController, Post } from 'routing-controllers';
+import { SignUpDto, SignInDto } from './auth.dto';
+import User from '../module/user/schema/user.schema';
+import { AuthorizationException } from '../module/auth/exceptions';
+import { AuthService } from '../module/auth/auth.service';
+import * as bcrypt from 'bcrypt';
+import { Service } from 'typedi';
+import 'reflect-metadata';
+
+@JsonController()
+@Service()
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('/register')
+    async register(@Body() body: SignUpDto): Promise<{ bearerToken: string, userId: string }> {
+        if (await User.findOne({ email: body.email })) {
+            throw new AuthorizationException('Пользователь с таким email уже существует');
+        }
+        if (await User.findOne({ login: body.login })) {
+            throw new AuthorizationException('Пользователь с таким login уже существует');
+        }
+        const user = new User({
+            login: body.login,
+            email: body.email,
+            password: await bcrypt.hash(body.password, 13),
+        });
+        await user.save();
+        return await this.authService.login(body.login, body.password);
+    }
+}
